58998e87423739ab6fc508e93946abc15acf8a43
Fiezt <dat09t@gmail.com>
2022-06-17 18:38:17 +0700

    add delete slash cmd

---
+++ commands/private/deleteSlash.js
@@ -0,0 +1,35 @@
+const { token, client_id, test_guild_id } = require("../../config");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v10");
+const rest = new REST({ version: "10" }).setToken(token);
+
+module.exports = {
+	name: "deleteslash",
+	description: "delete slash command",
+	category: "private",
+	// args: true,
+	ownerOnly: true,
+
+	async execute(message, args) {
+		const { client } = message;
+
+		await rest
+			.get(Routes.applicationGuildCommands(client_id, test_guild_id))
+			.then((data) => {
+				const promises = [];
+				for (const command of data) {
+					const deleteUrl = `${Routes.applicationGuildCommands(
+						client_id,
+						test_guild_id
+					)}/${command.id}`;
+					promises.push(rest.delete(deleteUrl));
+				}
+				return Promise.all(promises);
+			});
+
+		return message.reply("Done");
+
+		//Routes.applicationCommands(client_id)
+	},
+};
