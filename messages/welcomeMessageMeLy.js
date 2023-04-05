170bec9dc209fac19749482b395f9b9ec9db66e9
Fiezt <dat09t@gmail.com>
2023-04-05 20:29:01 +0700

    fix: bugs?

--- messages/welcomeMessageMeLy.js
+++ messages/welcomeMessageMeLy.js
@@ -7,7 +7,7 @@ module.exports = {
 	async execute(member) {
 		const { client, guild } = member;
 
-		const fetchedGuild = await guild.fetch();
+		// const fetchedGuild = await guild.fetch();
 
 		if (member.user.bot) {
 			const botRole =
@@ -25,27 +25,27 @@ module.exports = {
 			return;
 		}
 
-		if (client.string.validateZalgo(member.user.username)) {
-			let newName = client.removeZalgo(member.user.username);
-			try {
-				await member.setNickname(newName, "Tên không hợp lệ");
-				await member.send({
-					embeds: [
-						new Discord.EmbedBuilder()
-							.setColor("Red")
-							.setDescription(
-								`Vì username của cậu sẽ có thể gây rối trong công tác quản trị máy chủ **Code MeLy**. Nickname mới của cậu sẽ được đặt thành \`${newName}\`. Cậu có thể chọn thay đổi nickname mới hoặc giữ nguyên như vậy.\nNếu tên hiển thị của bạn vẫn còn không hợp lệ (chứa các ký tự khó để nhìn, không thể gõ như bình thường), MeLy phải buộc lòng mời cậu ra khỏi máy chủ.`
-							),
-					],
-				});
-			} catch (error) {
-				let fiezt = await client.users.fetch(owner);
-				await fiezt.send({
-					content: `${member.id}\nĐổi nickname: ${newName}\n\`\`\`${error.message}\`\`\``,
-				});
-				console.log(error);
-			}
-		}
+		// if (client.string.validateZalgo(member.user.username)) {
+		// 	let newName = client.removeZalgo(member.user.username);
+		// 	try {
+		// 		await member.setNickname(newName, "Tên không hợp lệ");
+		// 		await member.send({
+		// 			embeds: [
+		// 				new Discord.EmbedBuilder()
+		// 					.setColor("Red")
+		// 					.setDescription(
+		// 						`Vì username của cậu sẽ có thể gây rối trong công tác quản trị máy chủ **Code MeLy**. Nickname mới của cậu sẽ được đặt thành \`${newName}\`. Cậu có thể chọn thay đổi nickname mới hoặc giữ nguyên như vậy.\nNếu tên hiển thị của bạn vẫn còn không hợp lệ (chứa các ký tự khó để nhìn, không thể gõ như bình thường), MeLy phải buộc lòng mời cậu ra khỏi máy chủ.`
+		// 					),
+		// 			],
+		// 		});
+		// 	} catch (error) {
+		// 		let fiezt = await client.users.fetch(owner);
+		// 		await fiezt.send({
+		// 			content: `${member.id}\nĐổi nickname: ${newName}\n\`\`\`${error.message}\`\`\``,
+		// 		});
+		// 		console.log(error);
+		// 	}
+		// }
 
 		const welcomeChannel = await guild.channels.cache.find((c) =>
 			c.name.toLowerCase().includes("say-hi")
@@ -61,21 +61,21 @@ module.exports = {
 
 		const banner = await getRandomWelcomeImage(guild);
 
-		const langs = Object.keys(require("../constants/locale"));
-		langs.shift();
+		// const langs = Object.keys(require("../constants/locale"));
+		// langs.shift();
 
-		let lang = langs[Math.floor(Math.random() * langs.length)];
+		// let lang = langs[Math.floor(Math.random() * langs.length)];
 
-		let content = "Hello";
+		let content = `cout << "hello world, ${member}!";` ;
 
-		try {
-			content = await translate(content, { from: "en", to: lang });
-		} catch (error) {
-			console.log(error);
-			content = "Hello";
-		} finally {
-			content += ` ${member}!`;
-		}
+		// try {
+		// 	content = await translate(content, { from: "en", to: lang });
+		// } catch (error) {
+		// 	console.log(error);
+		// 	content = "Hello";
+		// } finally {
+		// 	content += ` ${member}!`;
+		// }
 
 		const row = new Discord.ActionRowBuilder().addComponents(
 			new Discord.ButtonBuilder()
