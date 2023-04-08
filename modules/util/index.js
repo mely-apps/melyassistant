0418efbf5a61ae2e142ce92ef60637549abd48d9
Fiezt <dat09t@gmail.com>
2023-04-08 11:51:45 +0700

    improved filter

--- modules/util/index.js
+++ modules/util/index.js
@@ -7,10 +7,20 @@ const getRandomWelcomeImage = async (guild) => {
 	const imagesChannelCache = await guild.channels.cache.find((c) =>
 		c.name.toLowerCase().includes("image-welcome")
 	);
+
 	const imagesChannel = await imagesChannelCache.fetch();
+
 	const messages = await imagesChannel.messages.fetch();
-	const message = messages.random();
-	return message.attachments.first();
+
+	const message = await messages
+		.filter((x) => x.attachments.size || !!x.attachments)
+		.random();
+
+	const banner = await message.attachments
+		.filter((x) => `${x.contentType}`.includes("image"))
+		.random();
+
+	return banner;
 };
 
 module.exports = { getRandomWelcomeImage };
